94605f6a7f5042a90e15ce95068ec7871068eb3a
Anna Jacobsen <aj4693@rit.edu>
2019-09-03 20:41:27 -0400

    I don't remember what i did last night.

--- src/components/health/index.jsx
+++ src/components/health/index.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import './health.css';
 import heart from '../../images/heart.png';
 
-const Health = () => {
+const Health = (props) => {
     return (
-        <div className="container">
-            <img src={heart} alt="A heart representing a health point."/>
-            <img src={heart} alt="A heart representing a health point."/>
-            <img src={heart} alt="A heart representing a health point."/>
-            <img src={heart} alt="A heart representing a health point."/>
+        <div className="container center-vertical">
+            <h4>GPA:</h4>
+            <div className="container hearts">
+                <img className="heart" src={heart} alt="A heart representing a health point."/>
+                <img className="heart" src={heart} alt="A heart representing a health point."/>
+                <img className="heart" src={heart} alt="A heart representing a health point."/>
+                <img className="heart" src={heart} alt="A heart representing a health point."/>
+            </div>
         </div>
     )
 }
+
+export default Health;
